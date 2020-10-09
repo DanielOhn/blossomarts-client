20b79e181c53229989b896a59d7095e953972c52
Daniel Ohn <ohndaniel@gmail.com>
2020-10-09 16:38:28 -0700

    Add Twitter and Instagram svg icons.  Add Footer component.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+import "../styles/Footer.css"
+
+import Twitter from "../icons/social/twitter"
+import Instagram from "../icons/social/instagram"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <Twitter />
+      <Instagram />
+    </div>
+  )
+}
+
+export default Footer
